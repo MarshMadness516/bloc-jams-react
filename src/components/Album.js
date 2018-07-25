e47242fabab2b4f97ce39ff9086386afc91aa258
unknown <mpritchett516@gmail.com>
2018-07-25 12:03:17 -0400

    implemented next button functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -70,9 +70,23 @@ class Album extends Component {
   }
 
   handlePrevClick() {
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong ===song);
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
     const newIndex = Math.max(0, currentIndex -1);
     const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length, currentIndex + 1);
+    var newSong;
+    if (newIndex === this.state.album.songs.length) {
+      return;
+    }
+    else {
+      newSong = this.state.album.songs[newIndex];
+    }
     this.setSong(newSong);
     this.play();
   }
@@ -113,6 +127,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
